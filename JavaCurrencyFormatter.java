5023a16579141f4314314ae487d6267b2229bdaf
joseMeurer <josewmeurer@gmail.com>
2023-10-12 09:37:08 -0300

    folder organization

---
+++ JavaCurrencyFormatter.java
@@ -0,0 +1,34 @@
+package Java;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class JavaCurrencyFormatter {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        // Write your code here.
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
+    }
+}
+
+/*
+https://www.hackerrank.com/challenges/java-currency-formatter/problem?isFullScreen=true
+
+https://docs.oracle.com/javase/8/docs/api/java/text/NumberFormat.html
+https://docs.oracle.com/javase/8/docs/api/java/text/NumberFormat.html#getCurrencyInstance-java.util.Locale-
+https://docs.oracle.com/javase/8/docs/api/java/util/Locale.html#Locale-java.lang.String-java.lang.String-
+ */
+
