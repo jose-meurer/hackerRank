1d72be5ae4be7a67d4185e488808b3fa3e0b2178
joseMeurer <josewmeurer@gmail.com>
2023-11-06 16:14:19 -0300

    Print in Reverse

---
+++ Print in Reverse.js
@@ -0,0 +1,113 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+const SinglyLinkedListNode = class {
+    constructor(nodeData) {
+        this.data = nodeData;
+        this.next = null;
+    }
+};
+
+const SinglyLinkedList = class {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    insertNode(nodeData) {
+        const node = new SinglyLinkedListNode(nodeData);
+
+        if (this.head == null) {
+            this.head = node;
+        } else {
+            this.tail.next = node;
+        }
+
+        this.tail = node;
+    }
+};
+
+function printSinglyLinkedList(node, sep) {
+    while (node != null) {
+        process.stdout.write(String(node.data));
+
+        node = node.next;
+
+        if (node != null) {
+            process.stdout.write(sep);
+        }
+    }
+}
+
+/*
+ * Complete the 'reversePrint' function below.
+ *
+ * The function accepts INTEGER_SINGLY_LINKED_LIST llist as parameter.
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+function reversePrint(llist) {
+    // Write your code here
+    const stack = [];
+    
+    let node = llist;
+    while(node !== null) {
+        stack.push(node.data);
+        node = node.next;
+    }
+   
+    stack.reverse().forEach((x) => {
+        console.log(x);
+    });
+}
+
+function main() {
+    const tests = parseInt(readLine(), 10);
+
+    for (let testsItr = 0; testsItr < tests; testsItr++) {
+        const llistCount = parseInt(readLine(), 10);
+
+        let llist = new SinglyLinkedList();
+
+        for (let i = 0; i < llistCount; i++) {
+            const llistItem = parseInt(readLine(), 10);
+            llist.insertNode(llistItem);
+        }
+
+        reversePrint(llist.head);
+    }
+}
+
+/* 
+https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list-in-reverse/problem?isFullScreen=true
+*/
